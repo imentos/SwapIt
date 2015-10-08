0bfffdf79f7652930afcb0db0be41fdd226fe25f
Ray Kuo <ray.kuo@sap.com>
2015-10-07 22:34:31 -0700

    Make phone call and send email

--- iphone/ChangeIt/ChangeIt/ItemDetailController.swift
+++ iphone/ChangeIt/ChangeIt/ItemDetailController.swift
@@ -9,8 +9,9 @@
 import UIKit
 import Parse
 import Social
+import MessageUI
 
-class ItemDetailController: UIViewController {
+class ItemDetailController: UIViewController, MFMailComposeViewControllerDelegate {
     @IBOutlet var backToUserButton: UIBarButtonItem!
     @IBOutlet weak var exchangeImage: UIImageView!
     @IBOutlet weak var photoImage: UIImageView!
@@ -65,11 +66,44 @@ class ItemDetailController: UIViewController {
     @IBAction func usePhone(sender: AnyObject) {
         let phone = self.userJSON["phone"].string
         print(phone)
+        if let url = NSURL(string: "tel://\(phone)") {
+            UIApplication.sharedApplication().openURL(url)
+        }
     }
     
     @IBAction func useEmail(sender: AnyObject) {
         let email = self.userJSON["email"].string
         print(email)
+        
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.presentViewController(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+    }
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients([self.userJSON["email"].string!])
+        mailComposerVC.setSubject("Sending you an in-app e-mail...")
+        mailComposerVC.setMessageBody("Sending e-mail in-app is not so bad!", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate
+    
+    func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+        
     }
     
     func updateCommunications() {
