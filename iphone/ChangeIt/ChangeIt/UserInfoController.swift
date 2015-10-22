877198755be3b9e0491f0348579521f431e5e64f
Ray Kuo <ray.kuo@sap.com>
2015-10-22 11:16:54 -0700

    Add user info page

---
+++ iphone/ChangeIt/ChangeIt/UserInfoController.swift
@@ -0,0 +1,21 @@
+//
+//  UserInfoController.swift
+//  ChangeIt
+//
+//  Created by Kuo, Ray on 10/22/15.
+//  Copyright © 2015 i818292. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoController: UIViewController {
+
+    @IBOutlet var webview: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = NSURL(string: "http://brttr.com/main.html")
+        self.webview.loadRequest(NSURLRequest(URL: url!))
+    }
+}
